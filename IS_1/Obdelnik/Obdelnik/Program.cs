3144bddd6ea8139f611b1b07cbc68b27f767a075
Kateřina Daňková <katul.dankova@seznam.cz>
2022-10-20 10:14:00 +0200

    IS 1

---
+++ IS_1/Obdelnik/Obdelnik/Program.cs
@@ -0,0 +1,29 @@
+﻿var key = ConsoleKey.Enter;
+while (key == ConsoleKey.Enter)
+{
+    Console.Clear();
+    int a = 0;
+    int b = 0;
+    while (a == 0)
+    {
+        Console.WriteLine("Zadejte pozadovany pocet radku: ");
+        int.TryParse(Console.ReadLine(), out a);
+    }
+    while (b == 0)
+    {
+        Console.WriteLine("Zadejte pozadovany pocet sloupcu: ");
+        int.TryParse(Console.ReadLine(), out b);
+    }
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            Console.Write("*");
+        }
+        Console.Write("\n");
+    }
+
+    Console.WriteLine("\nPro opakovani stisknete ENTER");
+    key = Console.ReadKey().Key;
+}
