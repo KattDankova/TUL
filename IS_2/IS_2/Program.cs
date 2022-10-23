44098ceb38ffacff5ba413e88566dc9c1aa53c53
Kateřina Daňková <katerina.dankova@pslib.cz>
2022-10-23 17:29:50 +0200

    IS 2

--- IS_2/IS_2/Program.cs
+++ IS_2/IS_2/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var key = ConsoleKey.Enter;
+while (key == ConsoleKey.Enter)
+{
+    Console.Clear();
+    int vyska = 0;
+    while (vyska == 0)
+    {
+        Console.WriteLine("Zadejte vysku trojuhelniku: ");
+        int.TryParse(Console.ReadLine(), out vyska);
+    }
+
+    //Pravoúhlý
+    //for (int i = 1; i <= vyska; i++)
+    //{
+    //    for (int j = 0; j < i; j++)
+    //    {
+    //        Console.Write("*");
+    //    }
+    //    Console.Write("\n");
+    //}
+
+    //Rovnoramenný
+    //int starCounter = 1;
+    //for (int i = 0; i < vyska; i++)
+    //{
+    //    int counter = vyska - i;
+    //    while (counter > 1)
+    //    {
+    //        Console.Write(" ");
+    //        counter--;
+    //    }
+    //    for (int j = 0; j < starCounter; j++)
+    //    {
+    //        Console.Write("*");
+    //    }
+    //    starCounter += 2;
+    //    Console.Write("\n");
+    //}
+
+    //Stromeček
+    int layer = 1;
+    for (int i = 0; i < vyska; i++)
+    {
+        int starCounter = 1;
+        for (int k = 0; k < layer; k++)
+        {
+            int counter = vyska - k;
+            while (counter > 1)
+            {
+                Console.Write(" ");
+                counter--;
+            }
+            for (int j = 0; j < starCounter; j++)
+            {
+                Console.Write("*");
+            }
+            starCounter += 2;
+            Console.Write("\n");
+        }
+        layer++;
+    }
+
+    Console.WriteLine("\nPro opakovani stisknete ENTER");
+    key = Console.ReadKey().Key;
+}
+
