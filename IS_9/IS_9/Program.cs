74241d28600e32d5d57bd261a52ef40629fbf945
Kateřina Daňková <katul.dankova@seznam.cz>
2022-11-30 12:07:36 +0100

    IS 9 (4, 14)

---
+++ IS_9/IS_9/Program.cs
@@ -0,0 +1,45 @@
+﻿int sirka = 8;
+int vyska = 8;
+
+Console.WriteLine("\n");
+
+//4
+for (int i = 0; i <= vyska; i++)
+{
+    for (int j = 0; j < sirka; j++)
+    {
+        if (i == 0 || i == vyska - 1)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            if (j == vyska - i - 1)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+    Console.Write("\n");
+}
+
+//14
+for (int i = 0; i < vyska; i++)
+{
+    for (int j = 0; j < sirka; j++)
+    {
+        if (i < vyska / 2 && j < sirka / 2 || i >= vyska / 2 && j >= sirka / 2)
+        {
+            Console.Write(" ");
+        }
+        else
+        {
+            Console.Write("*");
+        }
+    }
+    Console.Write("\n");
+}
